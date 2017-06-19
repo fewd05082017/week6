d70d160dbc100fbfbfcc647479a4a00db7fc1471
kcontreras01 <kiara@Kiaras-MacBook-Pro.local>
2017-06-19 17:02:41 -0400

    First Draft

---
+++ js/project.js
@@ -0,0 +1,21 @@
+// $("button").on("click", function(){
+// 	$(this).find("ipsum").addClass("current");
+// });
+
+// function popIpsum(){
+// 	// populate chosen ipsum
+// 	$(#ipsum).find(.data());
+// }
+// You will likely need to use all of the following 
+// jQuery functions: .click(), .focus(), .removeClass(), .addClass(), 
+// .data(), .slice(), .find(), .hide(), .slideDown(), .slideUp()
+
+// $(a).on("click", function(pickIpsum){
+// 	$(nav).find("data-ipsum").addClass("current");
+
+// });
+
+
+$(a).on("click", function(){
+	$(this).addClass(.current);
+});
